f85dc4766a7dfd666ae5845969e87e0c2d5da193
Srijan Singh <srijan.singh.1232@gmail.com>
2021-01-09 19:08:59 +0530

    build

---
+++ build/precache-manifest.e954efe1382500924ce6f892fb6d8030.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c5de4030762fc73efaf203abbe767a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b245b277928bf3fac71",
+    "url": "/static/css/main.e6306054.chunk.css"
+  },
+  {
+    "revision": "821b14833c1d1f95d1bd",
+    "url": "/static/js/2.fce5fe14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fce5fe14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b245b277928bf3fac71",
+    "url": "/static/js/main.abf9f04a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "51186ac7dc384ed2dddc8c2afdc31ccb",
+    "url": "/static/media/background.51186ac7.jpg"
+  },
+  {
+    "revision": "e611c9d2e4aaaea3ba65a88c190ba726",
+    "url": "/static/media/category1.e611c9d2.jpg"
+  },
+  {
+    "revision": "997593adc6dbc606c2daa65da51befa9",
+    "url": "/static/media/category2.997593ad.jpg"
+  },
+  {
+    "revision": "997593adc6dbc606c2daa65da51befa9",
+    "url": "/static/media/category3.997593ad.jpg"
+  },
+  {
+    "revision": "e611c9d2e4aaaea3ba65a88c190ba726",
+    "url": "/static/media/category4.e611c9d2.jpg"
+  },
+  {
+    "revision": "a0e80bfb2f15eda0ff4b0ec94cf0a4e7",
+    "url": "/static/media/category5.a0e80bfb.jpg"
+  },
+  {
+    "revision": "e3f16d1ded9d31896fb5e44629974f03",
+    "url": "/static/media/category6.e3f16d1d.jpg"
+  },
+  {
+    "revision": "906db9fd54683cf29f9a2758c0b99af9",
+    "url": "/static/media/featuremock.906db9fd.png"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/google.db9b21a1.png"
+  },
+  {
+    "revision": "dfcc24af39c57240844718107ecc18f2",
+    "url": "/static/media/grocery1.dfcc24af.png"
+  },
+  {
+    "revision": "825e2353b434f314c72fa724b7eb0b10",
+    "url": "/static/media/grocery2.825e2353.png"
+  },
+  {
+    "revision": "72e39b5b976d0416ea0123d62785b07f",
+    "url": "/static/media/grocery3.72e39b5b.png"
+  },
+  {
+    "revision": "8b855912c8ca8f98923b775c21f06c2b",
+    "url": "/static/media/grocery4.8b855912.png"
+  },
+  {
+    "revision": "a20c6e0c4605210981ade52803a81884",
+    "url": "/static/media/grocery5.a20c6e0c.png"
+  },
+  {
+    "revision": "ba1d6d8090a7b78296743fcf1a1b7e9e",
+    "url": "/static/media/money.ba1d6d80.png"
+  },
+  {
+    "revision": "27987c0e9917c0d0d94d55242a314385",
+    "url": "/static/media/return.27987c0e.png"
+  },
+  {
+    "revision": "16476283f3e3d80149c083437ac3f8c5",
+    "url": "/static/media/supermarket.16476283.png"
+  },
+  {
+    "revision": "b877ea85a683548a8658aafce95ef77e",
+    "url": "/static/media/truck.b877ea85.png"
+  }
+]);
